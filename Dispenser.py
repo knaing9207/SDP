70d1d4d434ac01a1c41f1db653b21723b7c9f4c6
knaing9207 <knaing9207@gmail.com>
2023-11-11 13:55:50 -0500

    First Commit

---
+++ Dispenser.py
@@ -0,0 +1,25 @@
+import RPi.GPIO as GPIO
+import time
+
+servoPIN = 13
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servoPIN, GPIO.OUT)
+
+p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
+p.start(12.5) # Initialization
+try:
+    while True:
+        p.ChangeDutyCycle(0)
+        user = input("Dispenser Pill?: (yes/no) ")
+        if user == "yes":
+            p.ChangeDutyCycle(2.5)
+            time.sleep(1)
+            p.ChangeDutyCycle(12.5)
+            time.sleep(1)
+        elif user == "no":
+            pass
+        else:
+            print("Try Again")
+except KeyboardInterrupt:
+  p.stop()
+  GPIO.cleanup()
---
+++ Pill_Info.py
@@ -0,0 +1,4 @@
+class pillinfo:
+    def __init__(self, name = None, quantity = 0) -> None:
+        self.name = name
+        self.quantity = quantity
