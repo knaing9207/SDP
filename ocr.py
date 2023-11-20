eeb53d9d37514db51c5a148803b365719753f878
knaing9207 <knaing9207@gmail.com>
2023-11-20 09:43:04 -0500

    change

--- ocr.py
+++ ocr.py
@@ -4,7 +4,7 @@ from PIL import Image
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 # Open the image file
-image = Image.open('edges.jpg')
+image = Image.open('drug1.jpg')
 
 # Perform OCR using PyTesseract
 text = pytesseract.image_to_string(image)
